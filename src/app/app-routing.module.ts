957041a8c6c586a027323d0f850792d0425b4645
Brinthan <brinthsega@gmail.com>
2020-11-08 15:47:29 +0530

    home ui

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
+import { HomeComponent } from './home/home.component';
+import { TvshowsListComponent } from './tvshows-list/tvshows-list.component';
+import { MovieListComponent } from './movie-list/movie-list.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'movies', component: MovieListComponent },
+  { path: 'tv-shows', component: TvshowsListComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,19 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavigationComponent } from './navigation/navigation.component';
 import { CarouselComponent } from './carousel/carousel.component';
+import { MovieListComponent } from './movie-list/movie-list.component';
+import { TvshowsListComponent } from './tvshows-list/tvshows-list.component';
+import { HomeComponent } from './home/home.component';
+import { HttpClient } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavigationComponent,
-    CarouselComponent
+    CarouselComponent,
+    MovieListComponent,
+    TvshowsListComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
